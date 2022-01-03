0ec90dc4b6f4c18eb6fbf7cace41bae9c6561681
sander frenken <>
2022-01-03 16:44:40 +0100

    wip

--- scripts/content.js
+++ scripts/content.js
@@ -4,9 +4,27 @@ const description = "dasdasdasdasdasdas";
 const historyTitle = "History";
 const history = "Add some storytelling here. Not the scenario of your game, but rather some backgrounds behind the creation process: why are you making this game? Most projects starts with a cool story.";
 
+const images = [
+  "game_1",
+  "game_2",
+  "game_3",
+  "game_4",
+  "home",
+  "lego",
+  "queensday",
+  "store",
+  "creator",
+  "farm",
+  "overworld",
+  "village",
+  "heroes",
+  "presentation"
+];
+
 function displayContent() {
   displayDescription();
   displayHistory();
+  displayImagesForGallery();
 }
 
 function displayHistory() {
@@ -15,4 +33,13 @@ function displayHistory() {
 
 function displayDescription() {
   document.getElementById('grid_description').innerHTML = `<h2>${descriptionTitle}</h2><p>${description}</p>`;
+}
+
+function displayImagesForGallery() {
+  let innerHTML = "";
+  for (index in images) {
+    const image = images[index];
+    innerHTML += `<div class="gallery__item"><a href="images/${image}.png"><img src="images/${image}.png" alt="${image}"></a></div>`;
+  }
+  document.getElementById('gallery_items').innerHTML = innerHTML;
 }
